f96e5fbca5cc6462ee8e75047dfd460bc6d7c803
Marcelo Dias <marceloobig@outlook.com>
2021-03-22 11:25:02 -0300

    ok
    Finalizado

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Conversor
- Convertendo dólaras em reias
+ <h1> Convertendo dólaras em reias </h1>
+
+
+
+<img src="01gif.gif">
+
+
+--- 
+
+
+## Utilizando :
+  - HTML5
+  - CSS3
+  - JS
---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+var ps5 = prompt("Valor do ps5 em dolar");
+
+var ps5EmReais = parseInt(ps5);
+
+ ps5EmReais = ps5EmReais * 5.50;
+
+ alert(`O preço do PS5 no Brasil é R$${ps5EmReais} reais.`);
